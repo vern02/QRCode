21d833b3eaf6e7a721d33d6cd012fc09519979b0
vernsl <107494341+vern02@users.noreply.github.com>
2024-11-03 20:54:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,13 @@ with st.sidebar:
             "nav-link-selected": {"font-weight": "bold"}
         }
     )
+
+col1, col2, col3 = st.columns([1, 1, 1])
+
+with col2:
+    st.image("PhishUp_Logo.png")
     
-st.markdown("<h1 style='text-align: center; color: #00FFFF;'>🔍 Phish Up 🔵</h1>", unsafe_allow_html=True)
+# st.markdown("<h1 style='text-align: center; color: #00FFFF;'>🔍 Phish Up 🔵</h1>", unsafe_allow_html=True)
 
 # Function to predict email text
 def predict_email_text(text):
